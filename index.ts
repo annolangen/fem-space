5e0bc9f56404b88cb11952b2ce944e5dc9c9be93
Anno Langen <anno.langen@gmail.com>
2025-07-27 10:35:02 -0700

    Mobile destination

--- index.ts
+++ index.ts
@@ -1,14 +1,18 @@
 import { render, html } from "lit-html";
 import data from "./data.json";
 
+const state = {
+  step: 1,
+  destination: data.destinations[0],
+  crew: data.crew[0],
+  technology: data.technology[0],
+};
+
 const headerHtml = () =>
   html` <header
-    class="relative flex flex-row justify-between items-center w-full p-16"
+    class="relative flex flex-row justify-between items-center w-full px-6 pt-6"
   >
-    <img
-      src=${new URL("./assets/shared/logo.svg", import.meta.url)}
-      alt="logo"
-    />
+    <img src="/assets/shared/logo.svg" alt="logo" />
     <input type="checkbox" id="menu" class="peer sr-only" />
     <label for="menu" id="menu-button" class="z-20"></label>
     <nav
@@ -24,12 +28,12 @@ const headerHtml = () =>
 const homeHtml = () => html`
   <div
     id="home"
-    class="h-screen flex flex-col justify-between items-center text-white"
+    class="h-screen flex flex-col justify-between items-center text-white bg-cover bg-center bg-no-repeat"
   >
     ${headerHtml()}
     <main class="m-8 flex flex-col items-center gap-8">
       <div class="text-lg">So, you want to travel to</div>
-      <div class="text-9xl">SPACE</div>
+      <div class="text-8xl font-serif">SPACE</div>
       <div class="text-center">
         You’ll never have enough time. Space Let’s face it; if you want to go to
         space, you might as well genuinely go to outer space and not hover kind
@@ -48,10 +52,59 @@ const homeHtml = () => html`
 const destinationHtml = () =>
   html`<div
     id="destination"
-    class="h-screen flex flex-col justify-between items-center text-white"
+    class="h-screen flex flex-col items-center text-white bg-cover bg-center bg-no-repeat"
   >
     ${headerHtml()}
-    <main></main>
+    <main
+      class="m-6 flex flex-col items-center gap-6 h-[85vh] max-w-lg text-blue-200"
+    >
+      <h2 class="text-lg uppercase tracking-widest  opacity-50">
+        <span class="font-bold mr-2">01</span> Pick your destination
+      </h2>
+
+      <div class="flex-grow flex items-center justify-center min-h-0">
+        <img
+          class="max-h-full max-w-full object-contain"
+          src=${state.destination.images.png}
+          alt=${state.destination.name}
+        />
+      </div>
+
+      <div class="flex flex-col items-center gap-4 opacity-50">
+        <nav class="flex flex-row">
+          ${data.destinations.map(
+            destination =>
+              html`<button
+                @click=${() => {
+                  state.destination = destination;
+                  renderBody();
+                }}
+                class="mx-4 uppercase tracking-widest pb-2 ${destination ==
+                state.destination
+                  ? "border-b-2 border-white"
+                  : "border-b-2 border-transparent hover:border-white/50"}"
+              >
+                ${destination.name}
+              </button>`
+          )}
+        </nav>
+        <h1 class="text-6xl md:text-8xl font-serif uppercase">
+          ${state.destination.name}
+        </h1>
+        <p class="text-center max-w-md px-4">
+          ${state.destination.description}
+        </p>
+        <hr class="w-full border-white/25 my-4" />
+        <div class="flex flex-col md:flex-row gap-8 md:gap-16">
+          <div class="text-center">
+            <div class="text-sm  uppercase tracking-widest">Avg. Distance</div>
+            <div class="text-2xl uppercase font-serif">
+              ${state.destination.distance}
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
   </div>`;
 
 const bodyHtml = () => {
--- republish.sh
+++ republish.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-DEST=~/github/annolangen.github.io/rock-paper-scissors
+DEST=~/github/annolangen.github.io/space
 
 rm -rf dist .parcel-cache $DEST/*
 npm run build
 cp dist/* $DEST
 cd $DEST/..
 git add .
-git commit -m 'Republish countries'
+git commit -m 'Republish space'
 git push
